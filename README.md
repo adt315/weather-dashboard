61b79f1ec176116d627dc9fe6a4de976f1d646fa
adt315 <angela_dyan@hotmail.com>
2020-09-20 00:06:49 -0400

    updates

--- README.md
+++ README.md
@@ -1,49 +1,28 @@
 ## Weather Dashboard
 
-The goal of this project was to build a weather dashboard that This app runs in the browser and features dynamically updated HTML and CSS..  The OpenWeather API (https://openweathermap.org/api) was used to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+The goal of this project was to build a weather dashboard with dynamically updated HTML and CSS.
+  
+The OpenWeather API (https://openweathermap.org/api) was used to retrieve weather data for cities. 
 
 ### How It Works:
 
-**WHEN I search for a city I am presented with current and future conditions for that city and that city is added to the search history
+- When the user searches for a city in the search field current weather information for that city is displayed
 
-**Current weather conditions for that city include the city name, date, icon, temperature, humidity, wind speed, and UV index
+- Current weather information provided to the user includes the city name, date, icon, temperature, humidity, wind speed, and UV index
 
-**WHEN I view the UV index I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+- The UV Index field is colored to show if the conditions are favorable, moderate, or severe
 
-**I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+- When the user searches for a city they are also presented with a 5-day forecast for that city
 
-**WHEN I click on a city in the search history I am again presented with current and future conditions for that city
+- Information from the 5-day forecast includes the date, a weather condition icon, the temperature, and the humidity
 
-**WHEN I open the weather dashboard I am presented with the last searched city forecast
+- After the city is entered it is added to the search history
 
-----------------------------------------------------------------------------------------------------------------------------------------
+- When the user clicks on a city in the search history they are provided with current weather and a future forecast for that city
 
-
-
-
-WHEN I search for a city THEN I am presented with current and future conditions for that city and that city is added to the search history
-USER CAN SEARCH FOR WEATHER REPORTS BY CITY USING THE OPENWEATHERMAP API
-
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view current weather conditions for that city THEN I am presented with the city name, the date, , 
-THE FOLLOWING INFORMATION IS DISPLAYED: CURRENT TEMPERATURE, CURRENT HUMIDITY, WINDSPEED, UV INDEX, AND 5 DAY FORECAST
-
-APPLICATION USES ICONS TO REPRESENT WEATHER CONDITIONS(an icon representation of weather conditions)
-
-
-WHEN I view the UV index THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-
-WHEN I view future weather conditions for that city THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-
-
-WHEN I click on a city in the search history THEN I am again presented with current and future conditions for that city
-APPLICATION STORES PREVIOUSLY SEARCHED FOR CITIES IN LOCAL STORAGE AND DISPLAYS THEM TO THE USER
-
-
-WHEN I open the weather dashboard THEN I am presented with the last searched city forecast
-APPLICATION LOADS LAST SEARCHED CITY FORECAST ON PAGE LOAD
+- When the weather dashboard is opened weather information for the last city searched is provided
 
 
 https://adt315.github.io/weather-dashboard/
+
+![Screenshot (133)](https://user-images.githubusercontent.com/51940058/93693421-af080700-facd-11ea-9628-b4d865ad23d5.png)
--- script.js
+++ script.js
@@ -1,118 +1,132 @@
 $(document).ready(function () {
 
+    //localStorage.clear();
+    var lastSearchedCity = localStorage.getItem(localStorage.key(0));
+    weatherSearch(lastSearchedCity); //passes an actual variable searchValue
+
     $("#search-btn").on("click", function () {
         var searchValue = $("#city-search").val();
         $("#city-search").val(""); //clear search field
-        function storeCities(){
-            myCities = [];
-            myCities.push(searchValue);
-            localStorage.setItem(myCities, JSON.stringify(myCities));
-            // console.log(myCities);
-        }
-        storeCities();
         weatherSearch(searchValue); //passes an actual variable searchValue
-        getForecast(searchValue);
-        getMyCities();
     });
 
+    $(".city-btn").on("click", function () {
+        var selectedCity = $(this).attr("city-name");
 
-        function getMyCities() {
-            for (i = 0; i < localStorage.length; i++) {
-            myCities = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        console.log("City Button Press");
 
-          console.log(myCities);
-                  $(".list-group-item").text(myCities[i]);  
-    // //         var li = document.createElement("li");
-    // //         li.textContent = Object.values(myCities);
+        weatherSearch(selectedCity); //passes an actual variable searchValue
+        getForecast(selectedCity);
+    });
 
-                 }
-     }
-    
+    function getMyCities() {
+        console.log("Storage Length " + localStorage.length);
 
+        $("#cities-list").empty();
+        for (i = 0; i < localStorage.length; i++) {
+            var myCities = localStorage.getItem(localStorage.key(i));
 
+            console.log(myCities);
 
+            var citySearchList = $("<button>");
+            citySearchList.addClass("city-btn").attr("city-name", myCities).text(myCities);
+            $("#cities-list").append(citySearchList);
+        }
+    }
 
     function weatherSearch(searchValue) {  //this searchValue is a placeholder
         $.ajax({
-            type: "GET",
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=a468ca0d8bd9b0455127f58ae98ac8b8&units=imperial",
-            success: function (data) {
-                console.log(data);
+            method: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=a468ca0d8bd9b0455127f58ae98ac8b8&units=imperial"
+        }).done(function (data) {
+            console.log(data);
 
-                $("#current-weather").empty();
-
-                var cardTitle = $("<h4>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
-                var card = $("<div>").addClass("today-card");
-                var icon = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-                var temp = $("<p>").addClass("temp").text("Temperature: " + data.main.temp + " °F");
-                var humidity = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
-                var windSpeed = $("<p>").addClass("wind-speed").text("Wind Speed: " + data.wind.speed + " MPH");
-                var uvIndex = $("<p>").addClass("uv-index").text("UV Index: ");
-                var cardBody = $("<div>").addClass("card-body");
-
-                cardTitle.append(icon);
-                cardBody.append(cardTitle, temp, humidity, windSpeed, uvIndex);
-                card.append(cardBody);
-                $("#current-weather").append(card);
-            }
-        })
+            $("#current-weather").empty();
+            localStorage.setItem(searchValue, searchValue);
+            getForecast(searchValue);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            getUV(lat, lon, data);
+            getMyCities();
+        });
     }
-
-    
-
-    // var lat = data.coord.lat; 
-    // var lon = data.coord.lon;
-    
-    
-    // function getUV() {  
-    //     $.ajax({
-    //         type: "GET",
-    //         url:"https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=a468ca0d8bd9b0455127f58ae98ac8b8",
-    //         // success: function(results) {
-    //         //     console.log(uvIndex);
-    //     })
-    // }
-    // getUV();
-
-    // // <p class="uv-index">UV Index: </p>
-
-    //     })
-    // }
-    //     getUV();
-
 
     function getForecast(searchValue) {  //this searchValue is a placeholder
         $.ajax({
-            type: "GET",
-            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=a468ca0d8bd9b0455127f58ae98ac8b8&units=imperial",
-            success: function (data) {
-                console.log(data);
-                $(".card-title-1").text(data.list[7].dt_txt); //splice
-                // var icon = $(".icon-1").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-                $(".icon-1").text("icon");
-                $(".temp-1").text("Temp: " + data.list[0].main.temp + "°F");
-                $(".humidity-1").text("Humidity: " + data.list[0].main.humidity + "%");
+            method: "GET",
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=a468ca0d8bd9b0455127f58ae98ac8b8&units=imperial"
+        }).then(function (data) {
+            console.log(data);
+            $(".card-title-1").text(data.list[7].dt_txt); //splice
+            var icon1 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.list[7].weather[0].icon + ".png");
+            // 
+            var cardTitle1 = $(".card-title-1")
+            cardTitle1.append(icon1);
+            $(".temp-1").text("Temp: " + data.list[0].main.temp + "°F");
+            $(".humidity-1").text("Humidity: " + data.list[0].main.humidity + "%");
 
-                $(".card-title-2").text(data.list[15].dt_txt);
-                $(".icon-2").text("icon");
-                $(".temp-2").text("Temp: " + data.list[1].main.temp + "°F");
-                $(".humidity-2").text("Humidity: " + data.list[1].main.humidity + "%");
+            $(".card-title-2").text(data.list[15].dt_txt);
+            var icon2 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.list[15].weather[0].icon + ".png");
+            var cardTitle2 = $(".card-title-2")
+            cardTitle2.append(icon2);
+            $(".icon-2").text("icon");
+            $(".temp-2").text("Temp: " + data.list[1].main.temp + "°F");
+            $(".humidity-2").text("Humidity: " + data.list[1].main.humidity + "%");
 
-                $(".card-title-3").text(data.list[23].dt_txt);
-                $(".icon-3").text("icon");
-                $(".temp-3").text("Temp: " + data.list[2].main.temp + "°F");
-                $(".humidity-3").text("Humidity: " + data.list[2].main.humidity + "%");
+            $(".card-title-3").text(data.list[23].dt_txt);
+            var icon3 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.list[23].weather[0].icon + ".png");
+            var cardTitle3 = $(".card-title-3")
+            cardTitle3.append(icon3);
+            $(".temp-3").text("Temp: " + data.list[2].main.temp + "°F");
+            $(".humidity-3").text("Humidity: " + data.list[2].main.humidity + "%");
 
-                $(".card-title-4").text(data.list[31].dt_txt);
-                $(".icon-4").text("icon");
-                $(".temp-4").text("Temp: " + data.list[3].main.temp + "°F");
-                $(".humidity-4").text("Humidity: " + data.list[3].main.humidity + "%");
+            $(".card-title-4").text(data.list[31].dt_txt);
+            var icon4 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.list[31].weather[0].icon + ".png");
+            var cardTitle4 = $(".card-title-4")
+            cardTitle4.append(icon4);
+            $(".temp-4").text("Temp: " + data.list[3].main.temp + "°F");
+            $(".humidity-4").text("Humidity: " + data.list[3].main.humidity + "%");
 
-                $(".card-title-5").text(data.list[39].dt_txt);
-                $(".icon-5").text("icon");
-                $(".temp-5").text("Temp: " + data.list[4].main.temp + "°F");
-                $(".humidity-5").text("Humidity: " + data.list[4].main.humidity + "%");
+            $(".card-title-5").text(data.list[39].dt_txt);
+            var icon5 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.list[39].weather[0].icon + ".png");
+            var cardTitle5 = $(".card-title-5")
+            cardTitle5.append(icon5);
+            $(".temp-5").text("Temp: " + data.list[4].main.temp + "°F");
+            $(".humidity-5").text("Humidity: " + data.list[4].main.humidity + "%");
+        });
+    }
+
+    function getUV(lat, lon, data) {
+        $.ajax({
+            method: "GET",
+            url: "https://api.openweathermap.org/data/2.5/uvi?appid=a468ca0d8bd9b0455127f58ae98ac8b8&lat=" + lat + "&lon=" + lon
+        }).done(function (uvData) {
+            console.log(uvData);
+            console.log(uvData.value);
+
+            var cardTitle = $("<h4>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+            var card = $("<div>").addClass("today-card");
+            var icon = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+            var temp = $("<p>").addClass("temp").text("Temperature: " + data.main.temp + " °F");
+            var humidity = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+            var windSpeed = $("<p>").addClass("wind-speed").text("Wind Speed: " + data.wind.speed + " MPH");
+            var uvIndex = $("<p>").addClass("uv-index").text("UV Index: " + uvData.value);
+
+            if (uvData.value < 3) {
+                uvIndex.addClass("favorable");
             }
-        })
+            if (3 >= uvData.value >= 7) {
+                uvIndex.addClass("moderate");
+            }
+            if (uvData.value > 7) {
+                uvIndex.addClass("severe");
+            }
+
+            var cardBody = $("<div>").addClass("card-body");
+            cardTitle.append(icon);
+            cardBody.append(cardTitle, temp, humidity, windSpeed, uvIndex);
+            card.append(cardBody);
+            $("#current-weather").append(card);
+        });
     }
 });
